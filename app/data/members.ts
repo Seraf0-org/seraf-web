895ff2c8cac6abb9e8ae9e74abdb73b2346559f3
KTN44295080 <koutyan44295080@icloud.com>
2025-05-01 16:35:29 +0900

    新メンバーの情報を追加しました。

--- app/data/members.ts
+++ app/data/members.ts
@@ -22,7 +22,7 @@ export const members: Member[] = [
     {
         id: 1,
         name: "KTN",
-        position: "代表, ゲームデザイナー, シナリオライター, エンジニア, モデラー",
+        position: "代表, ゲームデザイナー, シナリオライター, エンジニア, モデラー、映像クリエイター",
         mainImage: "/images/members/member-1.jpg",
         subImage: "/images/members/member-1-sub.jpg",
         description: "たくさんゲームを作るます。\nゲームデザイン、シナリオ制作をメインに手広く活動していきます。",
@@ -95,7 +95,7 @@ export const members: Member[] = [
     {
         id: 4,
         name: "Jiska",
-        position: "デザイナー, 映像作家",
+        position: "デザイナー, 映像クリエイター",
         mainImage: "/images/members/member-4.jpg",
         subImage: "/images/members/member-4.jpg",
         description: "準備中。",
@@ -134,7 +134,7 @@ export const members: Member[] = [
     {
         id: 6,
         name: "しそ",
-        position: "デザイナー",
+        position: "UIデザイナー",
         mainImage: "/images/members/member-6.jpg",
         subImage: "/images/members/member-6.jpg",
         description: "準備中。",
@@ -153,7 +153,7 @@ export const members: Member[] = [
     {
         id: 7,
         name: "あきべ",
-        position: "イラストレーター",
+        position: "イラストレーター、デザイナー",
         mainImage: "/images/members/member-7.jpg",
         subImage: "/images/members/member-7.jpg",
         description: "準備中。",
@@ -172,7 +172,7 @@ export const members: Member[] = [
     {
         id: 8,
         name: "rapid",
-        position: "モデラー",
+        position: "モデラー、リガー",
         mainImage: "/images/members/member-8.jpg",
         subImage: "/images/members/member-8.jpg",
         description: "準備中。",
@@ -184,6 +184,33 @@ export const members: Member[] = [
                 color: {
                     base: "6, 182, 212",
                     hover: "8, 145, 178"
+                }
+            }
+        ]
+    },
+    {
+        id: 9,
+        name: "前田ネイト",
+        position: "イラストレーター、キャラクターデザイン、背景画",
+        mainImage: "/images/members/member-9.jpg",
+        subImage: "/images/members/member-9.jpg",
+        description: "画家です。\n第96回新構造展奨励賞、第1回Gates Art Competition審査員特別賞、第4回ホキ美術館大賞展入選など実績多数。\nめざましテレビ「キラビト」、NHK「沼ハマ」、ニコニコニュース等にて紹介されました。\nオンライン芸術教室を運営。",
+        skills: ["iBisPaint", "Procreate", "InfinitePainter", "artset4", "油彩", "アクリル", "水彩", "日本画"],
+        sns: [
+            {
+                url: "https://x.com/_N_eko_",
+                label: "X(旧Twitter)",
+                color: {
+                    base: "6, 182, 212",
+                    hover: "8, 145, 178"
+                }
+            },
+            {
+                url: "https://www.instagram.com/cat_painter__n/",
+                label: "Instagram",
+                color: {
+                    base: "214, 41, 118",
+                    hover: "150, 47, 191"
                 }
             }
         ]
